8996c60e8b62e7f7759456aaab3edf8d91d0c0fd
Anson <all197@ucsd.edu>
2020-02-04 15:32:58 -0800

    infrastructure changes

---
+++ src/pages/course-selection.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+// This page will need to dynamically update what courses are shown
+// based on the user that is currently logged on
+const CourseSelection = () => {
+  
+}
+
+export default CourseSelection;
