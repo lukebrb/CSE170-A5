f344ef681bd2c09acaa44595267195b02fa58dec
Luke <lburbidg@ucsd.edu>
2020-02-06 20:42:22 -0800

    Add mini calendar selection and breadcrumbs

--- src/pages/page2-.js
+++ src/pages/page2-.js
@@ -2,11 +2,28 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
+import MiniCalendar from "../components/mini-calendar"
 import AvailabilityList from "../components/availability-list"
 
 const SecondPage = () => (
   <Layout>
+    <nav class="breadcrumb" aria-label="breadcrumbs">
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/course-selection">Courses</Link>
+        </li>
+        <li class="is-active">
+          <a href="#" aria-current="page">
+            Time Selection
+          </a>
+        </li>
+      </ul>
+    </nav>
     <h1>Choose A Time</h1>
+    <MiniCalendar />
     <AvailabilityList></AvailabilityList>
   </Layout>
 )
