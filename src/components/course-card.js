426a86212632d22ea1bbd6d49a774c45e8d9129f
Luke <lburbidg@ucsd.edu>
2020-02-06 21:10:14 -0800

    Add react-loadable to wrap apexcharts with

--- src/components/course-card.js
+++ src/components/course-card.js
@@ -1,34 +1,41 @@
 import React from "react"
 
+import Loadable from "react-loadable"
 import ReactApexChart from "react-apexcharts"
 
-const Card = props => (
-  <div 
-    style = {{
-      height: '150px',
-      margin: '2%',
-      border: 'solid',
-      borderRadius: '20px',
-      backgroundColor: '#fcf9d4',
-      display: 'flex',
+const LoadableChart = Loadable({
+  loader: () => import("react-apexcharts"),
+  loading() {
+    return <div>Loading...</div>
+  },
+})
 
+const Card = props => (
+  <div
+    style={{
+      height: "150px",
+      margin: "2%",
+      border: "solid",
+      borderRadius: "20px",
+      backgroundColor: "#fcf9d4",
+      display: "flex",
     }}
   >
     {/* Left side color */}
-    <div 
-      style = {{
-        height: '100%',
-        width: '40px',
+    <div
+      style={{
+        height: "100%",
+        width: "40px",
         backgroundColor: props.color,
-        borderRadius: '20px 0 0 20px'
+        borderRadius: "20px 0 0 20px",
       }}
     />
-    
+
     {/* Main details on the card */}
-    <div 
-      style = {{
-        padding: '3% 4%',
-        lineHeight: '5px'
+    <div
+      style={{
+        padding: "3% 4%",
+        lineHeight: "5px",
       }}
     >
       <h3>{props.id}</h3>
@@ -37,21 +44,23 @@ const Card = props => (
         <li>Total: {props.totalSlots}</li>
         <li>Available: {props.availSlots}</li>
       </ul>
-      
     </div>
 
     {/*Pie Chart */}
-    <div 
-      style = {{
-        position: 'absolute',
-        right: '0',
+    <div
+      style={{
+        position: "absolute",
+        right: "0",
       }}
     >
-      <ReactApexChart 
-        options={pieData.options} 
-        series={[ parseInt(props.availSlots) / parseInt(props.totalSlots), 
-                 (parseInt(props.totalSlots) - parseInt(props.availSlots)) / parseInt(props.totalSlots) ]} 
-        type="donut" 
+      <LoadableChart
+        options={pieData.options}
+        series={[
+          parseInt(props.availSlots) / parseInt(props.totalSlots),
+          (parseInt(props.totalSlots) - parseInt(props.availSlots)) /
+            parseInt(props.totalSlots),
+        ]}
+        type="donut"
       />
     </div>
   </div>
@@ -59,28 +68,31 @@ const Card = props => (
 
 const pieData = {
   options: {
-    labels: ['Available', 'Taken'],
-    colors: ['#00eeff', '#e60008'],
+    labels: ["Available", "Taken"],
+    colors: ["#00eeff", "#e60008"],
     chart: {
-      type: 'donut',
+      type: "donut",
     },
-    responsive: [{
-      breakpoint: 480,
-      options: {
-        chart: {
-          width: 200
+    responsive: [
+      {
+        breakpoint: 480,
+        options: {
+          chart: {
+            width: 200,
+          },
+          legend: {
+            position: "bottom",
+          },
         },
-        legend: {
-          position: 'bottom'
-        }
-      }
-    }],
-    dataLabels: { 
-      enabled: false, 
-      formatter: (val, opt) => {return val}
+      },
+    ],
+    dataLabels: {
+      enabled: false,
+      formatter: (val, opt) => {
+        return val
+      },
     },
   },
-  
 }
 
-export default Card;
+export default Card
