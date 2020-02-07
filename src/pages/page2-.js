79708005e6a034562926fbfeb16283b31d25ca80
Luke <lburbidg@ucsd.edu>
2020-02-06 20:23:46 -0800

    Add TimeSelection view to page 2 (temporary)

--- src/pages/page2-.js
+++ src/pages/page2-.js
@@ -2,14 +2,12 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import SEO from "../components/seo"
+import AvailabilityList from "../components/availability-list"
 
 const SecondPage = () => (
   <Layout>
-    <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
+    <h1>Choose A Time</h1>
+    <AvailabilityList></AvailabilityList>
   </Layout>
 )
 
