1a0a27161d11cea23a3ffe13baf38979bd57ee11
Anson <all197@ucsd.edu>
2020-02-11 22:11:34 -0800

    fixed errors from merge

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,9 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+import CourseSelection from './course-selection'
+import LoginPage from './login'
+
 import { isLoggedIn } from '../auth'
 
 function HomePage () {
