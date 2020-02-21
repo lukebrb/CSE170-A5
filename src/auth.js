07cb2abe680664bed8899ed161eaebd6a65245e5
Luke <lburbidg@ucsd.edu>
2020-02-21 13:29:56 -0800

    fix window.localstorage dependency on build

--- src/auth.js
+++ src/auth.js
@@ -1,24 +1,31 @@
 export const getUser = () => {
   if (typeof window !== "undefined") {
-    return window.localStorage.getItem('user') ? JSON.parse(window.localStorage.getItem('user')) : {};
-  } 
+    return window.localStorage.getItem("user")
+      ? JSON.parse(window.localStorage.getItem("user"))
+      : {}
+  }
 }
 
 export const isLoggedIn = () => {
-  return window.localStorage.getItem('user');
+  if (typeof window !== "undefined") {
+    return window.localStorage.getItem("user")
+  }
 }
 
 export const setUser = user => {
-  if ( typeof window !== "undefined" ) {
-    window.localStorage.setItem("user", JSON.stringify(user));
+  if (typeof window !== "undefined") {
+    window.localStorage.setItem("user", JSON.stringify(user))
   }
 }
 
 export const logout = firebase => {
   return new Promise(resolve => {
-    firebase.auth().signOut().then(function() {
-      window.localStorage.removeItem('user');
-      resolve();
-    })
+    firebase
+      .auth()
+      .signOut()
+      .then(function() {
+        window.localStorage.removeItem("user")
+        resolve()
+      })
   })
 }
