dd5571c4ecc46aa4f1680e1fc9788680f09688f8
Luke <lburbidg@ucsd.edu>
2020-03-14 15:58:05 -0700

    remove login text

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,19 +1,19 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Layout from '../components/layout';
+import SEO from '../components/seo';
 
-import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth'
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
 
-import { useFirebase } from 'gatsby-plugin-firebase'
-import { setUser } from '../auth'
+import { useFirebase } from 'gatsby-plugin-firebase';
+import { setUser } from '../auth';
 
 function LoginPage(props) {
   const [firebase, setFirebase] = useState();
 
   useFirebase(firebase => {
     setFirebase(firebase);
-  }, [])
+  }, []);
 
   const getUiConfig = auth => {
     return {
@@ -22,31 +22,29 @@ function LoginPage(props) {
       signInOptions: [
         auth.EmailAuthProvider.PROVIDER_ID,
         auth.GoogleAuthProvider.PROVIDER_ID,
-        auth.FacebookAuthProvider.PROVIDER_ID
+        auth.FacebookAuthProvider.PROVIDER_ID,
       ],
       callbacks: {
         signInSuccessWithAuthResult: result => {
           setUser(result.user);
           props.login(true);
-        }
-      }
-    }
-  }
+        },
+      },
+    };
+  };
 
   return (
     <Layout>
       <SEO title="Login" />
 
-      <h1>Login</h1>
-      
-      {firebase && 
-        <StyledFirebaseAuth 
-          uiConfig={getUiConfig(firebase.auth)} 
+      {firebase && (
+        <StyledFirebaseAuth
+          uiConfig={getUiConfig(firebase.auth)}
           firebaseAuth={firebase.auth()}
         />
-      }
+      )}
     </Layout>
-  )
+  );
 }
 
 export default LoginPage;
